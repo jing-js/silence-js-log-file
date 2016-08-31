c1853317256733e4f67a5151e7ad795dcce805d0
Yuhang <abeyuhang@gmail.com>
2016-08-31 20:02:25 +0800

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const format = require('util').format;
 const util = require('silence-js-util');
 const fs = require('fs');
 const path = require('path');
+const cluster = require('cluster');
 
 const LEVELS = {
   NONE: 4,
@@ -13,15 +14,32 @@ const LEVELS = {
   DEBUG: 0
 };
 const TIPS = ['[DEBUG]', '[INFO ]', '[WARN ]', '[ERROR]', '[NONE ]'];
-const MAX_CACHE_LENGTH = 1000;
+const MAX_CACHE_LENGTH = 100000; // 10万条可缓存在内存中, 当磁盘 IO 跟不上时。
 
-function fn(n) {
+
+// 不清楚琐碎的数字到字符串格式化对性能是否有影响。反正先生成好放那儿, 也占不了多少内存。
+const PAD_2_NUMS = (new Array(100).fill(0).map((n, i) => pad(i)));
+const PAD_3_NUMS = (new Array(1000).fill(0).map((n, i) => pad3(i)));
+const TIME_ZONE = (function () {
+  let to = (new Date()).getTimezoneOffset();
+  let tn = to < 0;
+  to = to < 0 ? -to : to;
+  let th = (to / 60) | 0;
+  let tm = (to - th * 60);
+  return ` GMT${tn ? '+' : '-'}${pad(th)}${pad(tm)}`
+})();
+
+function pad(n) {
   return n < 10 ? '0' + n : n.toString();
 }
 
-function formatDate(date, short = false, sep = '/') {
+function pad3(n) {
+  return n < 10 ? '00' + n : (n < 100 ? '0' + n : n.toString());
+}
+
+function formatDate(date) {
   date = date ? date : new Date();
-  return `${date.getFullYear()}${sep}${fn(date.getMonth() + 1)}${sep}${fn(date.getDate())}` + (short ? '' : ` ${fn(date.getHours())}:${fn(date.getMinutes())}:${fn(date.getSeconds())}`);
+  return `${date.getFullYear()}/${PAD_2_NUMS[date.getMonth()+1]}/${PAD_2_NUMS[date.getDate()]} ${PAD_2_NUMS[date.getHours()]}:${PAD_2_NUMS[date.getMinutes()]}:${PAD_2_NUMS[date.getSeconds()]}.${PAD_3_NUMS[date.getMilliseconds()]}${TIME_ZONE}`;
 }
 
 function _err(err) {
@@ -68,7 +86,7 @@ class Writer {
       this.d = d.getDate();
       let file = path.join(
         this.path,
-        `${this.y}-${fn(this.m + 1)}-${fn(this.d)}${this.postfix }.log`
+        `${this.y}-${PAD_2_NUMS[this.m + 1]}-${PAD_2_NUMS[this.d]}${this.postfix }.log`
       );
       this.stream = fs.createWriteStream(file, {
         flags: 'a'
@@ -139,7 +157,7 @@ class FileLogger {
     this._write(level, ...args);
   }
   _format(level, ...args) {
-    let prefix = `[${formatDate()}] ${TIPS[level]} `;
+    let prefix = (cluster.isWorker ? `[${cluster.worker.id}] ` : '') + `[${formatDate()}] ${TIPS[level]} `;
     if (args.length === 1) {
       return prefix + (typeof args[0] === 'object' ? JSON.stringify(args[0]) : args[0].toString()) + '\n';
     } else {
@@ -161,11 +179,12 @@ class FileLogger {
   warn(...args) {
     this._log(LEVELS.WARN, ...args);
   }
-  access(method, code, duration, ip, url) {
+  access(method, code, duration, bytesRead, bytesWritten, user, ip, url) {
     if (this.level === LEVELS.NONE) {
       return;
     }
-    this.accessWriter.write(`[${formatDate()}] [${code !== 0 && code < 1000 ? code : 200}] [${method}] [${duration}ms] [${ip}] ${url}\n`);
+    let ds = duration < 2000 ? duration + 'ms' : (duration / 1000 | 0) + 's';
+    this.accessWriter.write((cluster.isWorker ? `[${cluster.worker.id}] ` : '') + `[${formatDate()}] [${code !== 0 && code < 1000 ? code : 200}] [${method}] [${ds}] [${bytesRead}] [${bytesWritten}] [${user ? user : '-'}] [${ip}] ${url}\n`);
   }
   _write(level, ...args) {
     if (args.length === 0) {
