7b157d65345e29a13b6f1b74321b1e13f222c2c1
Yuhang <abeyuhang@gmail.com>
2016-11-08 15:20:22 +0800

    update

--- index.js
+++ index.js
@@ -16,10 +16,9 @@ const LEVELS = {
   DEBUG: 0,
 };
 const LEVEL_NAMES = ['DEBUG', 'INFO', 'WARN', 'ERROR', 'ACCESS', 'NONE'];
-const MAX_CACHE_LENGTH = 10000; // 1万条可缓存在内存中, 当磁盘 IO 跟不上时。
 
 function _err(err) {
-  console.log(err.stack || err.message || err.toString());
+  console.log(err);
 }
 
 class Writer {
@@ -36,15 +35,26 @@ class Writer {
     this._continueHandler = this._continue.bind(this);
     this._errorHandler = this._error.bind(this);
     this.stream = null;
-    this.MAX_CACHE_LENGTH = cfg.maxCache || MAX_CACHE_LENGTH;
+    this.fl = cfg.fl; // fallbackLogger
+    this.MAX_CACHE_LENGTH = cfg.maxCache;
   }
   _putCache(msg) {
-    if (this.cache.length > this.MAX_CACHE_LENGTH) {
-      _err(`FileLogger(level:${this.level}, section:${this.section}) cache received MAX_CACHE_LENGTH ${this.MAX_CACHE_LENGTH}`);
-      _err(msg);
+    // console.log(this.level, this.cache.length, this.MAX_CACHE_LENGTH)
+    if (this.cache.length >= this.MAX_CACHE_LENGTH) {
+      if (this.fl !== null) {
+        this.fl.serror('file-logger', this.level, this.section, 'cache received MAX_CACHE_LENGTH', this.MAX_CACHE_LENGTH);
+        this.fl._write(msg);
+      } else {
+        console.log('file-logger', this.level, this.section, 'cache received MAX_CACHE_LENGTH', this.MAX_CACHE_LENGTH);
+        console.log(msg);
+      }
     } else {
       this.cache.push(msg);
     }
+  }
+
+  get isAvaliable() {
+    return this.state === 0 || this.cache.length < this.MAX_CACHE_LENGTH;
   }
 
   write(msg) {
@@ -124,7 +134,8 @@ class FileLogger {
     this._accessWriter = null;
     this._maxCache = config.maxCache || MAX_CACHE_LENGTH;
     this._maxAccessCache = config.maxAccessCache || this._maxCache;
-    this._state = -1;  // -1: not init, 0: init and ready, 1: closed
+    this._state = -1;  // -1: not init, 0: init and ready, 1: closed,
+    this.fl = config.fallbackLogger || null;
   }
   get level() {
     return LEVEL_NAMES[this._level];
@@ -143,14 +154,12 @@ class FileLogger {
       level,
       section,
       path: this.path,
-      maxCache: level === 'ACCESS' ? this._maxAccessCache : this._maxCache
+      maxCache: level === 'ACCESS' ? this._maxAccessCache : this._maxCache,
+      fl: this.fl
     });
   }
 
-  init() {
-    if (this._state >= 0) {
-      return Promise.resolve();
-    }
+  _init() {
     this._state = 0;
     LEVEL_NAMES.forEach((level, idx) => {
       if (level === 'NONE') {
@@ -162,12 +171,23 @@ class FileLogger {
       this._writers[idx] = writer;
     });
     this._accessWriter = this._writers[LEVELS.ACCESS];
-
-    return util.mkdirP(this.path);
+    return this.path ? util.mkdirP(this.path) : Promise.resolve();
+  }
+  init() {
+    if (this._state >= 0) {
+      return Promise.resolve();
+    }
+    if (this.fl) {
+      return this.fl.init().then(() => {
+        return this._init();
+      });
+    } else {
+      return this._init();
+    }
   }
   close() {
     if (this._state > 0) {
-      return;
+      return Promise.resolve();
     }
     this._state = 1;
     let arr = [];
@@ -185,6 +205,9 @@ class FileLogger {
     });
     this._writers.length = 0;
     this._swriters.length = 0;
+
+    if (this.fl) arr.push(this.fl.close());
+
     return Promise.all(arr);
   }
   _format(level, args) {
@@ -248,41 +271,54 @@ class FileLogger {
     this._swrite(LEVELS.WARN, section, args);
   }
   access(method, code, duration, bytesRead, bytesWritten, user, clientIp, remoteIp, userAgent, url) {
-    if (LEVELS.ACCESS < this._level || this._state !== 0) {
+    if (LEVELS.ACCESS < this._level || this._state > 0) {
       return;
     }
-    let ds = duration < 2000 ? duration + 'ms' : (duration / 1000 | 0) + 's';
-    if (userAgent && userAgent.indexOf('"') >= 0) {
-      userAgent = userAgent.replace(/\"/g, '\\"')
+    if (this._state === 0 && this._accessWriter.isAvaliable) {
+      let ds = duration < 2000 ? duration + 'ms' : (duration / 1000 | 0) + 's';
+      if (userAgent && userAgent.indexOf('"') >= 0) {
+        userAgent = userAgent.replace(/\"/g, '\\"')
+      }
+      this._accessWriter.write(this._cluster + `[${util.formatDate()}] [${code !== 0 && code < 1000 ? code : 200}] [${method}] [${ds}] [${bytesRead}] [${bytesWritten}] [${user ? user : '-'}] [${clientIp || '-'}] [${remoteIp || '-'}] "${userAgent || '-'}" ${url}`);
+    } else if (this.fl !== null) {
+      this.fl.access(method, code, duration, bytesRead, bytesWritten, user, clientIp, remoteIp, userAgent, url);
+    } else {
+      console.log.apply(console, arguments);
     }
-    this._accessWriter.write(this._cluster + `[${util.formatDate()}] [${code !== 0 && code < 1000 ? code : 200}] [${method}] [${ds}] [${bytesRead}] [${bytesWritten}] [${user ? user : '-'}] [${clientIp || '-'}] [${remoteIp || '-'}] "${userAgent || '-'}" ${url}`);
   }
   _write(level, args) {
     if (args.length === 0) {
       return;
     }
-    let msg = this._format(level, args);
-    if (this._state === 0) {
+    if (this._state === 0 && this._writers[level].isAvaliable) {
+      let msg = this._format(level, args);
       this._writers[level].write(msg);
+    } else if (this.fl !== null) {
+      this.fl._write(level, args);
     } else {
-      console.log(msg);
+      console.log(...args);
     }
+  }
+  _getWriter(level, section) {
+    let ws = this._swriters[level];
+    let writer = ws.get(section);
+    if (!writer) {
+      writer = this._createWriter(LEVEL_NAMES[level], section);
+      ws.set(section, writer);
+    }
+    return writer;
   }
   _swrite(level, section, args) {
     if (args.length === 0) {
       return;
     }
-    let msg = this._format(level, args);
-    if (this._state === 0) {
-      let ws = this._swriters[level];
-      let writer = ws.get(section);
-      if (!writer) {
-        writer = this._createWriter(LEVEL_NAMES[level], section);
-        ws.set(section, writer);
-      }
-      writer.write(msg);
+    let wr;
+    if (this._state === 0 && (wr = this._getWriter(level, section)).isAvaliable) {
+      wr.write(this._format(level, args));
+    } else if (this.fl !== null) {
+      this.fl._swrite(level, section, args);
     } else {
-      console.log(msg);
+      console.log(...args);
     }
   }
 }
